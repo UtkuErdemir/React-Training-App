d0e4b83884b496fa563a5c30031cc17564b8258a
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-27 01:17:25 +0300

    Components are integrated to Units page

--- src/pages/Units/index.jsx
+++ src/pages/Units/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { segments } from '../../utils/constants'
+import ItemTitle from './components/ItemTitle'
+import Segment from './components/Segment'
 
 function Units () {
   return (
-    <div>Units</div>
+    <div>
+      <ItemTitle value={'Ages'}></ItemTitle>
+      <Segment items={segments}></Segment>
+      <ItemTitle value={'Costs'}></ItemTitle>
+    </div>
   )
 }
 
