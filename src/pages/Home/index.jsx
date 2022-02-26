fca4b1fdbfec1dbe1d08a3aff3c072e8fc91b044
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-26 21:26:21 +0300

    Homepage is imrpoved
    Image component is added to homepage.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from '../../components/Image'
+import './style.scss'
 
 function Home () {
+  const imageURL = 'https://www.ageofempires.com/wp-content/themes/ageOfEmpires/dist/images/heros/hero-homepage-4k.jpg'
   return (
-    <div>Home</div>
+    <div className='container'>
+        <Image src={imageURL} width={'60%'} height={'60%'}></Image>
+    </div>
   )
 }
 
