2d127dbbff0cf19a5959f2ed7543f69ec0c95cde
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-26 18:38:26 +0300

    CSS file is changed to SCSS
    CSS file is changed to SCSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
 import Units from './pages/Units'
 import Home from './pages/Home'
 import UnitDetail from './pages/UnitDetail'
@@ -9,6 +9,7 @@ function App () {
   return (
     <div className="App">
       <h2>React App</h2>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="units" element={<Units />} />
