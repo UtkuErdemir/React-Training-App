df21bb20aa329064e2e585b8dd34faf7721f9cc9
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-27 01:17:03 +0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+export const segments = [
+  {
+    title: 'All',
+    value: 'all'
+  },
+  {
+    title: 'Dark',
+    value: 'dark'
+  },
+  {
+    title: 'Feudal',
+    value: 'feudal'
+  },
+  {
+    title: 'Castle',
+    value: 'castle'
+  },
+  {
+    title: 'Imperial',
+    value: 'imperial'
+  }
+]
