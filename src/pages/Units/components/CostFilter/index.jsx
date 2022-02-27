ab61be2844a0668d8ed65469f6ffd0658bb62b0f
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-27 16:04:33 +0300

    Cost Filter title is added.

--- src/pages/Units/components/CostFilter/index.jsx
+++ src/pages/Units/components/CostFilter/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Checkbox from '../Checkbox'
 import Range from '../Range'
+import PropTypes from 'prop-types'
 import './style.scss'
 
-function CostFilter () {
+function CostFilter ({ title, onChange }) {
   return (
     <div className='cost-filter-container'>
-        <Checkbox title="Food"></Checkbox>
+        <Checkbox title={title}></Checkbox>
         <Range></Range>
     </div>
   )
+}
+
+CostFilter.propTypes = {
+  title: PropTypes.string,
+  onChange: PropTypes.func
 }
 
 export default CostFilter
