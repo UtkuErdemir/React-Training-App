56bd6262baade3ffe3e808a0ab5383e716757097
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-27 20:11:09 +0300

    Unit Detail is updated for selected unit.

--- src/pages/UnitDetail/index.jsx
+++ src/pages/UnitDetail/index.jsx
@@ -1,68 +1,79 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { changeAppTitle } from '../../utils/helpers'
 import Table from '../../components/Table'
 import TD from '../../components/Table/components/TD'
 import TR from '../../components/Table/components/TR'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllUnits } from '../../store/units/unitsActions'
 
 function UnitDetail () {
-  changeAppTitle('Unit Details')
+  const { data: units } = useSelector(state => state.units)
   const params = useParams()
   const id = params.id || ''
+  const selectedUnit = findSelectedUnitById(id, units)
+  const dispatch = useDispatch()
+  changeAppTitle('Unit Details')
+
+  useEffect(() => {
+    if (units.length === 0) { dispatch(getAllUnits()) }
+  }, [])
   return (
     <div>
-      <Table>
+      {units.length > 0 && <Table>
         <TR onPress={() => alert(id)}>
           <TD title='ID:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.id.toString()}></TD>
         </TR>
         <TR>
           <TD title='Name:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.name}></TD>
         </TR>
         <TR>
           <TD title='Description:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.description}></TD>
         </TR>
         <TR>
           <TD title='Min. Age:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.age}></TD>
         </TR>
         <TR>
           <TD title='Wood Cost:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.cost.Wood?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Food Cost:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.cost.Food?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Gold Cost:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.cost.Gold?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Build Time:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.build_time.toString()}></TD>
         </TR>
         <TR>
           <TD title='Reload Time:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.reload_time.toString()}></TD>
         </TR>
         <TR>
           <TD title='Hit Points:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.hit_points.toString()}></TD>
         </TR>
         <TR>
           <TD title='Attack:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.attack.toString()}></TD>
         </TR>
         <TR>
           <TD title='Accuracy:'></TD>
-          <TD title=''></TD>
+          <TD title={selectedUnit.accuracy.toString()}></TD>
         </TR>
-      </Table>
+      </Table>}
     </div>
   )
 }
+
+const findSelectedUnitById = (id, units) => units.find(unit => unit.id === parseInt(id))
 
 export default UnitDetail
