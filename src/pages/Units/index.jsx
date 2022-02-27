ea9c13160dd6c46639681b1bf6be57a04920a981
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-27 15:23:00 +0300

    Table is added to Units

--- src/pages/Units/index.jsx
+++ src/pages/Units/index.jsx
@@ -3,6 +3,11 @@ import { segments } from '../../utils/constants'
 import CostFilter from './components/CostFilter'
 import ItemTitle from './components/ItemTitle'
 import Segment from './components/Segment'
+import Table from './components/Table'
+import TD from './components/Table/components/TD'
+import TH from './components/Table/components/TH'
+import TR from './components/Table/components/TR'
+import './style.scss'
 
 function Units () {
   return (
@@ -13,6 +18,24 @@ function Units () {
       <CostFilter></CostFilter>
       <CostFilter></CostFilter>
       <CostFilter></CostFilter>
+      <ItemTitle value={'Result'}></ItemTitle>
+      <Table className="result-table">
+        <TR>
+          <TH title='sadasdsad'></TH>
+          <TH title='sadasdsad'></TH>
+          <TH title='sadasdsad'></TH>
+        </TR>
+        <TR>
+          <TD title='sadasdsad'></TD>
+          <TD title='sadasdsad'></TD>
+          <TD title='sadasdsad'></TD>
+        </TR>
+        <TR>
+          <TD title='sadasdsad'></TD>
+          <TD title='sadasdsad'></TD>
+          <TD title='sadasdsad'></TD>
+        </TR>
+      </Table>
     </div>
   )
 }
