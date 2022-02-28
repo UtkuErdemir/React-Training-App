3c363d2642cf8305d8545b1a63c2a54551c8e15d
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-28 22:23:35 +0300

    Filters are completed

--- src/pages/Units/index.jsx
+++ src/pages/Units/index.jsx
@@ -17,13 +17,28 @@ import './style.scss'
 function Units () {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const [selectedItem, setSelectedItem] = useState({})
+  const [selectedItem, setSelectedItem] = useState({ ...segments[0] })
+  const [costFilters, setCostFilters] = useState({})
+  const addCostFilter = (filterName, value) => setCostFilters(previous => {
+    const temp = { ...previous }
+    temp[filterName] = value
+    return temp
+  })
+  const removeCostFilter = (filterName) => setCostFilters(previous => {
+    const temp = { ...previous }
+    delete previous[filterName]
+    return temp
+  })
   const units = useSelector(state => {
     const initialData = state.units.data
     if (!selectedItem.value || selectedItem.value === 'all') return initialData
 
     const filteredDataByAge = [...initialData].filter(data => data.age?.toLowerCase() === selectedItem.value?.toLowerCase())
-    return filteredDataByAge
+    let filteredDataByCost = [...filteredDataByAge]
+    Object.entries(costFilters).forEach(([name, value]) => {
+      filteredDataByCost = filteredDataByCost.filter(data => data.cost && (data.cost[name] >= value || (!data.cost[name] && value === '0')))
+    })
+    return filteredDataByCost
   })
 
   changeAppTitle('Units')
@@ -36,9 +51,9 @@ function Units () {
       <ItemTitle value={'Ages'}></ItemTitle>
       <Segment onSelected={setSelectedItem} items={segments} value={selectedItem.value}></Segment>
       <ItemTitle value={'Costs'}></ItemTitle>
-      <CostFilter title={'Food'}></CostFilter>
-      <CostFilter title={'Wood'}></CostFilter>
-      <CostFilter title={'Gold'}></CostFilter>
+      <CostFilter title={'Food'} onChange={addCostFilter} removeFilter={removeCostFilter} value={costFilters.Food}></CostFilter>
+      <CostFilter title={'Wood'} onChange={addCostFilter} removeFilter={removeCostFilter} value={costFilters.Wood}></CostFilter>
+      <CostFilter title={'Gold'} onChange={addCostFilter} removeFilter={removeCostFilter} value={costFilters.Gold}></CostFilter>
       <ItemTitle value={'Result'}></ItemTitle>
       <Table className="result-table">
         <TR>
