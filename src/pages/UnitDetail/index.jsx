14d7435164bce70da8d2dfd309f4de178bcfb06d
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2022-02-28 21:23:08 +0300

    Filter method moved to selector.

--- src/pages/UnitDetail/index.jsx
+++ src/pages/UnitDetail/index.jsx
@@ -8,68 +8,73 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getAllUnits } from '../../store/units/unitsActions'
 
 function UnitDetail () {
-  const { data: units } = useSelector(state => state.units)
   const params = useParams()
   const id = params.id || ''
-  const selectedUnit = findSelectedUnitById(id, units)
+  const unit = useSelector(state => {
+    const initalUnits = state.units.data
+    const selectedUnit = findSelectedUnitById(id, initalUnits)
+    return selectedUnit
+  })
   const dispatch = useDispatch()
   changeAppTitle('Unit Details')
 
   useEffect(() => {
-    if (units.length === 0) { dispatch(getAllUnits()) }
+    if (!unit) { dispatch(getAllUnits()) }
   }, [])
   return (
     <div>
-      {units.length > 0 && <Table>
+      {unit &&
+      <Table>
         <TR onPress={() => alert(id)}>
           <TD title='ID:'></TD>
-          <TD title={selectedUnit.id.toString()}></TD>
+          <TD title={unit.id.toString()}></TD>
         </TR>
         <TR>
           <TD title='Name:'></TD>
-          <TD title={selectedUnit.name}></TD>
+          <TD title={unit.name}></TD>
         </TR>
         <TR>
           <TD title='Description:'></TD>
-          <TD title={selectedUnit.description}></TD>
+          <TD title={unit.description}></TD>
         </TR>
         <TR>
           <TD title='Min. Age:'></TD>
-          <TD title={selectedUnit.age}></TD>
+          <TD title={unit.age}></TD>
         </TR>
         <TR>
           <TD title='Wood Cost:'></TD>
-          <TD title={selectedUnit.cost.Wood?.toString()}></TD>
+          <TD title={unit.cost.Wood?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Food Cost:'></TD>
-          <TD title={selectedUnit.cost.Food?.toString()}></TD>
+          <TD title={unit.cost.Food?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Gold Cost:'></TD>
-          <TD title={selectedUnit.cost.Gold?.toString()}></TD>
+          <TD title={unit.cost.Gold?.toString()}></TD>
         </TR>
         <TR>
           <TD title='Build Time:'></TD>
-          <TD title={selectedUnit.build_time.toString()}></TD>
+          <TD title={unit.build_time.toString()}></TD>
         </TR>
         <TR>
           <TD title='Reload Time:'></TD>
-          <TD title={selectedUnit.reload_time.toString()}></TD>
+          <TD title={unit.reload_time.toString()}></TD>
         </TR>
         <TR>
           <TD title='Hit Points:'></TD>
-          <TD title={selectedUnit.hit_points.toString()}></TD>
+          <TD title={unit.hit_points.toString()}></TD>
         </TR>
         <TR>
           <TD title='Attack:'></TD>
-          <TD title={selectedUnit.attack.toString()}></TD>
+          <TD title={unit.attack.toString()}></TD>
         </TR>
         <TR>
           <TD title='Accuracy:'></TD>
-          <TD title={selectedUnit.accuracy.toString()}></TD>
+          <TD title={unit.accuracy.toString()}></TD>
         </TR>
-      </Table>}
+      </Table>
+      }
     </div>
   )
 }
